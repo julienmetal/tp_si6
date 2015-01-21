a1a1eaa614f44d6622b789c5d269429cb4ff349e
julienL <julien.lefevre.72@gmail.com>
2015-01-21 12:02:33 +0100

    fromage

---
+++ main.c
@@ -0,0 +1,20 @@
+int main();
+{
+double operande A;
+double operande B;
+double resultat*;
+double resultat/;
+
+scanf("Saisissez A: ");
+scanf(A);
+
+scanf("Saisissez A: ");
+scanf(B);
+
+resultat*= (A*B);
+printf(resultat*);
+
+resultat/= (A/B);
+printf(resultat/);
+
+}
