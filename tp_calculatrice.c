fcdc31476a59dd511c4c9700f4e53636332f5e8b
julienmetal <julien.lefevre.72@gmail.com>
2015-02-04 11:52:36 +0100

    Update tp_calculatrice.c
    Ajout d'un switch pour le choix de l'opération.

--- tp_calculatrice.c
+++ tp_calculatrice.c
@@ -12,6 +12,7 @@ double resultat*;
 double resultat/;
 double resultat+;
 double resultat-;
+string opération;
 
 //déclaration des fonctions
 
@@ -50,4 +51,30 @@ printf(resultat+);
 
 resultat-= add(A, B)
 printf(resultat-);
+}
+
+printf("Quel type d'opération souhaitez-vous faire ?");
+scanf(opération);
+
+switch(opération)
+{
+  case opération= "multiplication"
+  A*B;
+  break;
+  
+  case opération= "division"
+  A/B;
+  break;
+  
+  case opération= "addition"
+  A+B;
+  break;
+  
+  case opération= "soustraction"
+  A-B;
+  break;
+  
+  default:
+  printf("Erreur de saisie, veuillez saisir l'un des mots clés suivants: |multiplication| ou |division| ou |addition| ou |soustraction|")
+  break;
 }
