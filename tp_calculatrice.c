6b8745766ba5580fe22e747c1d411b25c6cb7bc5
julienmetal <julien.lefevre.72@gmail.com>
2015-02-22 15:42:17 +0100

    Update tp_calculatrice.c
    Finalisation du code.

--- tp_calculatrice.c
+++ tp_calculatrice.c
@@ -1,55 +1,75 @@
-# tp_si6
-#fromage
+#include <iostream>
+#include <string>
+using namespace std;
 
-#include <cstdint>
-int main();
+int main()
 {
 
-//Déclaration des variables
+//Déclaration des variables opérandes.
 double A;
 double B;
-double resultat*;
-double resultat/;
-double resultat+;
-double resultat-;
-string opération;
 
-//déclaration des fonctions
+//Déclaration des variables de résultat.
+double resultatMulti;
+double resultatDiv;
+double resultatAdd;
+double resultatSous;
+
+//Déclaration de la variable de type d'opération.
+int opération;
+
+//Déclaration et initialisation des variables d'opération.
+int multiplication = 1;
+int division = 2;
+int addition = 3;
+int soustraction = 4;
 
 
-printf("Veuillez saisir un nombre: ")
-scanf(A)
+//Saisie du premier nombre (opérande 1).
+cout<<"Veuillez saisir un nombre: "<<endl;
+cin>>A;
 
-printf("Veuillez saisir un autre nombre: ")
-scanf(B)
+//Saisie du deuxième nombre (opérande 2).
+cout<<"Veuillez saisir un autre nombre: "<<endl;
+cin>>B;
 
-printf("Quel type d'opération souhaitez-vous faire ?");
-scanf(opération);
+//Saisie du tyoe d'opération souhaité.
+cout<<"Quel type d'opération souhaitez-vous faire ? \n 1- Multiplication \n 2- Division \n 3- Addition \n 4- Soustraction"<<endl;
+cin>>opération;
+
+
+
+//Exécution de l'opération en fonction des paramètres précédemment saisis.
 
 switch(opération)
 {
-  case opération= "multiplication"
-  resultat* = A*B;
-  printf(resultat*);
+//Cas de multiplication.
+case 1:
+  resultatMulti = A*B;
+  cout<<"\n Le resultat de cette opération est: "<<resultatMulti<<endl;
   break;
   
-  case opération= "division"
-  resultat/ = A/B;
-  printf(resultat/);
+//Cas de division.
+case 2:
+  resultatDiv = A/B;
+  cout<<"\n Le resultat de cette opération est: "<<resultatDiv<<endl;
   break;
   
-  case opération= "addition"
-  resultat+ = A+B;
-  printf(resultat+);
+//Cas d'addition.
+case 3:
+  resultatAdd = A+B;
+  cout<<"\n Le resultat de cette opération est: "<<resultatAdd<<endl;
   break;
   
-  case opération= "soustraction"
-  resultat- = A-B;
-  printf(resultat-);
+//Cas de soustraction.
+case 4:
+  resultatSous = A-B;
+  cout<<"\n Le resultat de cette opération est: "<<resultatSous<<endl;
   break;
   
+
   default:
-  printf("Erreur de saisie, veuillez saisir l'un des mots clés suivants: |multiplication| ou |division| ou |addition| ou |soustraction|")
+  cout<<"Erreur de saisie. Vous devez saisir: 1 ou 2 ou 3 ou 4, selon l'opération que vous voulez effectuer."<<endl;
   break;
 }
 
