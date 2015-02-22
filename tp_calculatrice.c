e55b70dfd8f4054eb3f53a8ff20f7ce8b48a4ece
julienmetal <julien.lefevre.72@gmail.com>
2015-02-22 22:08:44 +0100

    Update tp_calculatrice.c
    Finalisations des commentaires/informations.

--- tp_calculatrice.c
+++ tp_calculatrice.c
@@ -5,35 +5,35 @@ using namespace std;
 int main()
 {
 
-//Déclaration des variables opérandes.
+//Déclaration des variables opérandes (type double).
 double A;
 double B;
 
-//Déclaration des variables de résultat.
+//Déclaration des variables de résultat (type double également).
 double resultatMulti;
 double resultatDiv;
 double resultatAdd;
 double resultatSous;
 
-//Déclaration de la variable de type d'opération.
+//Déclaration de la variable de type d'opération (type entier).
 int opération;
 
-//Déclaration et initialisation des variables d'opération.
+//Déclaration et initialisation des variables du type d'opération (type entier).
 int multiplication = 1;
 int division = 2;
 int addition = 3;
 int soustraction = 4;
 
 
-//Saisie du premier nombre (opérande 1).
+//Saisie du premier nombre, A(opérande 1).
 cout<<"Veuillez saisir un nombre: "<<endl;
 cin>>A;
 
-//Saisie du deuxième nombre (opérande 2).
+//Saisie du deuxième nombre, B (opérande 2).
 cout<<"Veuillez saisir un autre nombre: "<<endl;
 cin>>B;
 
-//Saisie du tyoe d'opération souhaité.
+//Saisie du type d'opération souhaité à l'aide de l'affichage d'un menu.
 cout<<"Quel type d'opération souhaitez-vous faire ? \n 1- Multiplication \n 2- Division \n 3- Addition \n 4- Soustraction"<<endl;
 cin>>opération;
 
@@ -67,7 +67,7 @@ case 4:
   cout<<"\n Le resultat de cette opération est: "<<resultatSous<<endl;
   break;
   
-
+//Tout autre cas que ceux précédemment définis: le programme retourne une erreur.
   default:
   cout<<"Erreur de saisie. Vous devez saisir: 1 ou 2 ou 3 ou 4, selon l'opération que vous voulez effectuer."<<endl;
   break;
