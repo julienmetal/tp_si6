f6b301db62d29d82a959a2aac2d1c82bcf55be8b
julienmetal <julien.lefevre.72@gmail.com>
2015-02-23 22:32:04 +0100

    Update tp_calculatrice.c
    Ajout de la documentaion doxygen

--- tp_calculatrice.c
+++ tp_calculatrice.c
@@ -1,3 +1,5 @@
+
+#include <cstdint>
 #include <iostream>
 #include <string>
 using namespace std;
@@ -5,35 +7,51 @@ using namespace std;
 int main()
 {
 
-//Déclaration des variables opérandes (type double).
-double A;
-double B;
+//Déclaration des variables opérandes.
+	///
+	/// \param B un double
+	/// \param A un double
+	///
+double A; 
+double B; 
 
-//Déclaration des variables de résultat (type double également).
+//Déclaration des variables de résultat.
+	///
+	/// \param resultatMulti un double
+	/// \param resultatDiv un double
+	/// \param resultatAdd un double
+	/// \param resultatSous un double
+		/// \return Le résultat de l'opération des deux opérandes en fonction de l'opérateur
+	///
 double resultatMulti;
-double resultatDiv;
-double resultatAdd;
-double resultatSous;
+double resultatDiv; 
+double resultatAdd; 
+double resultatSous; 
 
-//Déclaration de la variable de type d'opération (type entier).
+//Déclaration de la variable de type d'opération.
+	/// \param opération un entier 64bits
 int opération;
 
-//Déclaration et initialisation des variables du type d'opération (type entier).
-int multiplication = 1;
-int division = 2;
-int addition = 3;
-int soustraction = 4;
+//Déclaration et initialisation des variables d'opération.
+	/// \param multiplication un entier 64bits
+	/// \param division un entier 64bits
+	/// \param addition un entier 64bits
+	/// \param soustraction un entier 64bits
+int multiplication = 1; 
+int division = 2; 
+int addition = 3; 
+int soustraction = 4; 
 
 
-//Saisie du premier nombre, A(opérande 1).
+//Saisie du premier nombre (opérande 1).
 cout<<"Veuillez saisir un nombre: "<<endl;
 cin>>A;
 
-//Saisie du deuxième nombre, B (opérande 2).
+//Saisie du deuxième nombre (opérande 2).
 cout<<"Veuillez saisir un autre nombre: "<<endl;
 cin>>B;
 
-//Saisie du type d'opération souhaité à l'aide de l'affichage d'un menu.
+//Saisie du tyoe d'opération souhaité.
 cout<<"Quel type d'opération souhaitez-vous faire ? \n 1- Multiplication \n 2- Division \n 3- Addition \n 4- Soustraction"<<endl;
 cin>>opération;
 
@@ -67,7 +85,7 @@ case 4:
   cout<<"\n Le resultat de cette opération est: "<<resultatSous<<endl;
   break;
   
-//Tout autre cas que ceux précédemment définis: le programme retourne une erreur.
+
   default:
   cout<<"Erreur de saisie. Vous devez saisir: 1 ou 2 ou 3 ou 4, selon l'opération que vous voulez effectuer."<<endl;
   break;
